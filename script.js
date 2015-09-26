7d017e0624dc91fdc4a5f5869a1e88b2165e4803
Gilbert Smith <gjsmith3rd@gmail.com>
2015-09-25 20:26:28 -0500

    Styling changes to portfolio modal.

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ $(document).ready(function () {
 
     loadGallery(true, 'a.thumbnail');
 
-    //This function disables buttons when needed
-    function disableButtons(counterMax, counterCurrent) {
-        $('#show-previous-image, #show-next-image').show();
-        if (counterMax === counterCurrent) {
-            $('#show-next-image').hide();
-        } else if (counterCurrent === 1) {
-            $('#show-previous-image').hide();
-        }
-    }
-
     /**
     *
     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
@@ -35,24 +25,36 @@ $(document).ready(function () {
             updateGallery(selector);
         });
 
-        function updateGallery(selector) {
-            var $sel = selector;
-            currentImage = $sel.data('image-id');
-            $('#image-gallery-caption').text($sel.data('caption'));
-            $('#image-gallery-title').text($sel.data('title'));
-            $('#image-gallery-image').attr('src', $sel.data('image'));
-            $('#image-gallery-url').attr('href', $sel.data('url'));
-            disableButtons(counter, $sel.data('image-id'));
-        }
-
         if (setIDs === true) {
             $('[data-image-id]').each(function () {
                 counter++;
                 $(this).attr('data-image-id', counter);
             });
         }
+
         $(setClickAttr).on('click', function () {
             updateGallery($(this));
         });
+
+        function updateGallery(selector) {
+            var $sel = selector;
+            currentImage = $sel.data('image-id');
+            $('#image-gallery-caption').text($sel.data('caption'));
+            $('#image-gallery-title').text($sel.data('title'));
+            $('#image-gallery-image').attr('src', $sel.data('image'));
+            $('#image-gallery-header').attr('href', $sel.data('url'));
+            $('#image-gallery-body').attr('href', $sel.data('url'));
+            disableButtons(counter, $sel.data('image-id'));
+        }
+    }
+
+    //This function disables buttons when needed
+    function disableButtons(counterMax, counterCurrent) {
+        $('#show-previous-image, #show-next-image').show();
+        if (counterMax === counterCurrent) {
+            $('#show-next-image').hide();
+        } else if (counterCurrent === 1) {
+            $('#show-previous-image').hide();
+        }
     }
 });
